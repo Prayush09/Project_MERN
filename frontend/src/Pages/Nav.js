a58f8b7a581eff98e4f98a0d7af70cec8f0b34a0
Arpit Aditya <arpitaditya12100@gmail.com>
2023-11-11 23:02:08 +0530

    Fixed redirect to other website on clicking logo

--- frontend/src/Pages/Nav.js
+++ frontend/src/Pages/Nav.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 import logo from './Task Hub-logos_transparent.png';
 import './Nav.css'; // Import a custom CSS file
+import { useNavigate } from 'react-router-dom';
 
 function Nav() {
+  const navigate = useNavigate();
+  const home=()=>{
+    navigate("/sign-up");
+
+  }
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light custom-navbar">
-        <a className="navbar-brand" href="https://thenounproject.com/term/project-manager/4659573">
-          <img src={logo} alt="" width="50" height="50" />
+        <a className="navbar-brand">
+          <img src={logo} alt="" width="50" height="50"  onClick={home}/>
         </a>
 
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
