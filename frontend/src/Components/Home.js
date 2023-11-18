ab768a698e99593e4d6b8713b2d61bdb083a0001
Prayush Giri <prayushgiri@gmail.com>
2023-11-18 22:22:01 +0530

    new home and about pages with css

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,8 +1,49 @@
+import Nav from "../Pages/Nav";
+import './css/Home.css'
+import img1 from './icons/div1img.jpg';
+import img2 from './icons/div2img.jpg';
+import { useNavigate } from 'react-router-dom'; 
+
 function Home(){
+
+    const navigate = useNavigate();
+
+  const handleGetStartedClick = () => {
+    // Use the navigate function to go to the desired page
+    navigate('/sign-up');
+  };
+
+
     return(
-        <>
-        <h1>Home Page</h1>
-        </>
+        <div className="containerMain">
+            <Nav></Nav>
+            <div class="section-home1">
+            <img  src={img1}/>
+                <div className="text">
+                    <h1> Navigate success one project at a time - Simplify, Organize, Achieve!</h1>
+                </div>
+            </div>
+            <div class="section-home2">
+            <h1>Time management and Syncronized working environment results in better accountability, assessment, and improvment.</h1>
+            <img src={img2}></img>
+            
+            </div>
+            <br></br>
+            <div className="container-3">
+            <div className="section-home3">
+                <div className="section-home3-content">
+                    <h4 className="section-home3-title">Why delay? Take action now: Opportunities await ➡️</h4>
+                </div>
+
+                <div className="section-home3-logo-wrapper">
+                    <div style={{ padding: '20px' }}> <button className="but" onClick={handleGetStartedClick}>Get Started</button></div>
+                </div>
+            </div>
+            </div>
+            <br></br>
+        </div>
+        
+
     )
 }
 export {Home};
