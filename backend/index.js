5bdfdac2d43e0a2d26dfe36491c3ae0299cb797b
Prayush Giri <prayushgiri@gmail.com>
2023-11-20 19:19:34 +0530

    updating the mongoDB channel

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://prayushgiri:f7cSvj7TaenHfAFQ@cluster0.mfjfput.mongodb.net/')
+mongoose.connect('mongodb://localhost:27017/Project')
 let db=mongoose.connection;
 db.on('error',()=>console.log("Error in connecting to database"));
 db.on('open',()=>console.log("Connected to database"));
